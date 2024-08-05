50958b72925580217d59b015ffdbbc9906aa20d4
Aman Negi <amnnegi11@gmail.com>
2024-08-05 18:02:49 +0530

    frontend added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Groq = require("groq-sdk");
 const bodyParser = require('body-parser');
+const path = require('path');
 require('dotenv').config();
 
 const express = require('express');
@@ -44,6 +45,15 @@ app.post('/handle-prompt', async (req, res) => {
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
+});
+
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Basic route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
